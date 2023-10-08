0443e287545ea984f6ffe85f363bc051d7091131
jkea1 <jinkyng7890@gmail.com>
2023-10-08 23:07:49 +0900

    chore: 수정

--- nextjs-practice/src/app/products/[slug]/page.tsx
+++ nextjs-practice/src/app/products/[slug]/page.tsx
@@ -1,5 +1,5 @@
 import { getProduct, getProducts } from '@/service/products'
-import NotFoundProduct from '../not-found'
+import { notFound } from 'next/navigation'
 
 type Props = {
   params: {
@@ -7,28 +7,28 @@ type Props = {
   }
 }
 
-export function generateMetadata({ params }: Props) {
+export function generateMetadata({ params: { slug } }: Props) {
   return {
-    title: `제품의 이름: ${params.slug}`,
+    title: `제품의 이름: ${slug}`,
   }
 }
 
-export default function PantsPage({ params: { slug } }: Props) {
-  const product = getProduct(slug)
+export default async function ProductPage({ params: { slug } }: Props) {
+  const product = await getProduct(slug)
 
   if (!product) {
-    NotFoundProduct()
+    notFound()
   }
 
   // 서버 파일에 있는 데이터 중 해당 제품의 정보를 찾아서 그걸 보여줌
-  return <h1>{product} 제품 설명 페이지</h1>
+  return <h1>{product.name} 제품 설명 페이지</h1>
 }
 
-export function generateStaticParams() {
-  // 모든 제품의 페이지들을 미리 만들어 줄거다.()
-  const products = getProducts()
+export async function generateStaticParams() {
+  // 모든 제품의 페이지들을 미리 만들어 줄거다.(SSG)
+  const products = await getProducts()
 
   return products.map((product) => ({
-    slug: product, // params에 들어가는 형태로 반환해 준다.
+    slug: product.id, // params에 들어가는 형태로 반환해 준다.
   }))
 }
