75aefbd7542aff9b89314e991c2636698941fe93
jkea1 <jinkyng7890@gmail.com>
2023-10-08 22:07:05 +0900

    feat: data & api 로직 함수 적용

--- nextjs-practice/src/app/products/[slug]/page.tsx
+++ nextjs-practice/src/app/products/[slug]/page.tsx
@@ -1,4 +1,5 @@
-import { notFound } from 'next/navigation'
+import { getProduct, getProducts } from '@/service/products'
+import NotFoundProduct from '../not-found'
 
 type Props = {
   params: {
@@ -12,16 +13,21 @@ export function generateMetadata({ params }: Props) {
   }
 }
 
-export default function PantsPage({ params }: Props) {
-  if (params.slug === 'nothing') {
-    notFound()
+export default function PantsPage({ params: { slug } }: Props) {
+  const product = getProduct(slug)
+
+  if (!product) {
+    NotFoundProduct()
   }
 
-  return <h1>{params.slug} 제품 설명 페이지</h1>
+  // 서버 파일에 있는 데이터 중 해당 제품의 정보를 찾아서 그걸 보여줌
+  return <h1>{product} 제품 설명 페이지</h1>
 }
 
 export function generateStaticParams() {
-  const products = ['pants', 'skirt']
+  // 모든 제품의 페이지들을 미리 만들어 줄거다.()
+  const products = getProducts()
+
   return products.map((product) => ({
     slug: product, // params에 들어가는 형태로 반환해 준다.
   }))
--- nextjs-practice/src/app/products/page.tsx
+++ nextjs-practice/src/app/products/page.tsx
@@ -1,14 +1,17 @@
+import { getProducts } from '@/service/products'
 import Link from 'next/link'
 
-const products = ['shirt', 'pants', 'skirt', 'shoes']
-export default function ProductsPage() {
+export default async function ProductsPage() {
+  // 서버 파일(데이터베이스)에 있는 제품의 리스트를 읽어와서, 그걸 보여줌
+  const products = await getProducts()
+
   return (
     <>
       <h1>제품 소개 페이지</h1>
       <ul>
         {products.map((product, i) => (
           <li key={i}>
-            <Link href={`/products/${product}`}>{product}</Link>
+            <Link href={`/products/${product.id}`}>{product.name}</Link>
           </li>
         ))}
       </ul>
