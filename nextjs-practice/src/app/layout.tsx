7998a2dd486902c259ec942ac1629a3f69b6ded5
jkea1 <jinkyng7890@gmail.com>
2023-10-10 20:40:42 +0900

    feat: font

--- nextjs-practice/src/app/layout.tsx
+++ nextjs-practice/src/app/layout.tsx
@@ -1,6 +1,10 @@
 import { Metadata } from 'next'
+import { Nanum_Gothic, Open_Sans } from 'next/font/google'
 import Link from 'next/link'
 import styles from './layout.module.css'
+
+const sans = Open_Sans({ subsets: ['latin'] })
+const gothic = Nanum_Gothic({ weight: '700', subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: '멋진 제품 사이트',
@@ -16,10 +20,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang='en'>
+    <html lang='en' className={sans.className}>
       <body>
         <header className={styles.header}>
-          <h1>Demo Note App</h1>
+          <h1 className={gothic.className}>Demo Note App</h1>
           <nav className={styles.nav}>
             <Link href='/products'>Products</Link>
             <Link href='/about'>About</Link>
