a7f8f620d487ae46eea12db179df21af1849b676
jkea1 <jinkyng7890@gmail.com>
2023-10-10 20:40:26 +0900

    feat: redirect & rewrite

--- nextjs-practice/next.config.js
+++ nextjs-practice/next.config.js
@@ -1,4 +1,39 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/products/deleted_forever',
+        destination: '/products',
+        permanent: true,
+      },
+      {
+        source: '/products/deleted_temp',
+        destination: '/products',
+        permanent: false,
+      },
+    ]
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/ea',
+        destination: '/about/me/ea',
+      },
+      {
+        source: '/items/:slug',
+        destination: '/products/:slug', // key 정보가 들어있는 경우이다.
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
